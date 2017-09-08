55c8513fe5044318264737ccf5a23a8cb8b1cd23
Vlad <gimrcpp@gmail.com>
2017-09-08 15:23:00 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/ActivityMain.java
+++ android/src/rf/androidovshchik/vkadvancedposting/ActivityMain.java
@@ -1,16 +1,32 @@
 package rf.androidovshchik.vkadvancedposting;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ActivityMain extends AppCompatActivity implements AndroidFragmentApplication.Callbacks {
+
+	@BindView(R.id.stickersContainer)
+	ViewGroup stickersContainer;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
+	}
+
+	@OnClick(R.id.actionSticker)
+	public void onStickers() {
+		BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(stickersContainer);
+		bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 	}
 
 	@Override
