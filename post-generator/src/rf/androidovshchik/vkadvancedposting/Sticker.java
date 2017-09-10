f39bed1a4e07ea4b07a195ae0dc917e1cc18d8b2
Vlad <gimrcpp@gmail.com>
2017-09-10 21:39:24 +0300

    changes

---
+++ post-generator/src/rf/androidovshchik/vkadvancedposting/Sticker.java
@@ -0,0 +1,13 @@
+package rf.androidovshchik.vkadvancedposting;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Sticker extends Image {
+
+    public float startScale;
+
+    public Sticker(Texture texture) {
+        super(texture);
+    }
+}
--- post-generator/src/rf/androidovshchik/vkadvancedposting/StickersDragListener.java
+++ post-generator/src/rf/androidovshchik/vkadvancedposting/StickersDragListener.java
@@ -1,7 +1,6 @@
 package rf.androidovshchik.vkadvancedposting;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 
@@ -9,12 +8,17 @@ public class StickersDragListener extends InputListener {
 
     private static final String TAG = StickersDragListener.class.getSimpleName();
 
+    public static final int NONE = -1;
+
+    public int stickerIndex = NONE;
+
     private float startX, startY;
 
     @Override
     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
         if (pointer == 0) {
             Gdx.app.log(TAG, "touchDown x: " + x + " y: " + y);
+            stickerIndex = (Integer) event.getListenerActor().getUserObject();
             startX = x;
             startY = y;
         }
@@ -25,8 +29,15 @@ public class StickersDragListener extends InputListener {
     public void touchDragged(InputEvent event, float x, float y, int pointer) {
         if (pointer == 0) {
             Gdx.app.log(TAG, "touchDragged x: " + x + " y: " + y);
-            Actor actor = event.getListenerActor();
-            actor.setPosition(actor.getX() + (x - startX), actor.getY() + (y - startY));
+            event.getListenerActor().moveBy(x - startX, y - startY);
+        }
+    }
+
+    @Override
+    public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+        if (pointer == 0) {
+            Gdx.app.log(TAG, "touchUp x: " + x + " y: " + y);
+            stickerIndex = NONE;
         }
     }
 }
