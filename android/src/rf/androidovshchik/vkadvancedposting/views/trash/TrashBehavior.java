c05c103de2079117f0637ec9b3648ce0c25892fe
Vlad <gimrcpp@gmail.com>
2017-09-10 12:25:51 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/views/trash/TrashBehavior.java
+++ android/src/rf/androidovshchik/vkadvancedposting/views/trash/TrashBehavior.java
@@ -3,22 +3,67 @@ package rf.androidovshchik.vkadvancedposting.views.trash;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
-public class TrashBehavior extends CoordinatorLayout.Behavior<TrashFab> {
+import timber.log.Timber;
 
-    @SuppressWarnings("unused")
-    public TrashBehavior() {}
+@SuppressWarnings("unused")
+public class TrashBehavior extends CoordinatorLayout.Behavior<TrashFab>
+        implements GestureDetector.OnGestureListener {
 
-    @SuppressWarnings("unused")
+    private GestureDetector gestureDetector;
+
+    private boolean hasLongPressed = false;
+
     public TrashBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
+        gestureDetector = new GestureDetector(context, this);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Timber.d("Event: onDown");
+        hasLongPressed = false;
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {}
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent oldEvent, MotionEvent newEvent, float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent oldEvent, MotionEvent newEvent, float x, float y) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Timber.d("Event: onLongPress");
+        hasLongPressed = true;
     }
 
     @Override
     public boolean onInterceptTouchEvent(CoordinatorLayout coordinator, TrashFab child,
                                          MotionEvent event) {
-        child.animateTrash(event.getX(), event.getY());
+        gestureDetector.onTouchEvent(event);
+        if (hasLongPressed) {
+            if (event.getAction() != MotionEvent.ACTION_MOVE) {
+                Timber.d("Event action != ACTION_MOVE");
+                hasLongPressed = false;
+                return false;
+            }
+            child.animateTrash(event.getX(), event.getY());
+        }
         return false;
     }
 }
