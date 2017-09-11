647ff4edf85640a66ab7e876c99dc320cc9918a7
Vlad <gimrcpp@gmail.com>
2017-09-11 10:53:20 +0300

    changes

--- post-generator/src/rf/androidovshchik/vkadvancedposting/Sticker.java
+++ post-generator/src/rf/androidovshchik/vkadvancedposting/Sticker.java
@@ -5,12 +5,33 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Sticker extends Image {
 
-    public boolean isScaling = false;
-    public boolean isRotating = false;
+    public static final int NONE = -1;
 
-    public float startScale = 1f;
+    public int index;
 
-    public Sticker(Texture texture) {
+    public boolean isPinching = false;
+
+    public float startDragX;
+    public float startDragY;
+    public float startPinchX;
+    public float startPinchY;
+    public float startScale;
+    public float startRotation;
+
+    public Sticker(int index, Texture texture) {
         super(texture);
+        this.index = index;
+    }
+
+    public void setDragStarts(float x, float y) {
+        startDragX = x;
+        startDragY = y;
+    }
+
+    public void setPinchStarts() {
+        startPinchX = getX();
+        startPinchY = getY();
+        startScale = getScaleX();
+        startRotation = getRotation();
     }
 }
--- post-generator/src/rf/androidovshchik/vkadvancedposting/StickersDragListener.java
+++ post-generator/src/rf/androidovshchik/vkadvancedposting/StickersDragListener.java
@@ -8,19 +8,15 @@ public class StickersDragListener extends InputListener {
 
     private static final String TAG = StickersDragListener.class.getSimpleName();
 
-    public static final int NONE = -1;
-
-    public int stickerIndex = NONE;
-
-    private float startX, startY;
+    public int index = Sticker.NONE;
 
     @Override
     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
         if (pointer == 0) {
             Gdx.app.log(TAG, "touchDown pointer0 x: " + x + " y: " + y);
-            stickerIndex = (Integer) event.getListenerActor().getUserObject();
-            startX = x;
-            startY = y;
+            Sticker sticker = (Sticker) event.getListenerActor();
+            sticker.setDragStarts(x, y);
+            index = sticker.index;
         }
         return true;
     }
@@ -28,8 +24,11 @@ public class StickersDragListener extends InputListener {
     @Override
     public void touchDragged(InputEvent event, float x, float y, int pointer) {
         if (pointer == 0) {
-            Gdx.app.log(TAG, "touchDragged pointer0 x: " + x + " y: " + y);
-            event.getListenerActor().moveBy(x - startX, y - startY);
+            Sticker sticker = (Sticker) event.getListenerActor();
+            if (!sticker.isPinching) {
+                Gdx.app.log(TAG, "touchDragged pointer0 x: " + x + " y: " + y);
+                sticker.moveBy(x - sticker.startDragX, y - sticker.startDragY);
+            }
         }
     }
 
@@ -37,7 +36,7 @@ public class StickersDragListener extends InputListener {
     public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
         if (pointer == 0) {
             Gdx.app.log(TAG, "touchUp pointer0 x: " + x + " y: " + y);
-            stickerIndex = NONE;
+            index = Sticker.NONE;
         }
     }
 }
