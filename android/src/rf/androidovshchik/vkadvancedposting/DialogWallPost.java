3cd6826330a2ad2ae28bab4f983801d274e14970
Vlad <gimrcpp@gmail.com>
2017-09-12 19:51:06 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/DialogWallPost.java
+++ android/src/rf/androidovshchik/vkadvancedposting/DialogWallPost.java
@@ -1,11 +1,16 @@
 package rf.androidovshchik.vkadvancedposting;
 
+import android.animation.ObjectAnimator;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
 
 public class DialogWallPost extends DialogFragment {
 
@@ -22,12 +27,27 @@ public class DialogWallPost extends DialogFragment {
     }
 
     @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        return dialog;
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
         Window window = getDialog().getWindow();
         if (window != null) {
-            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT);
+            ObjectAnimator appearing = ObjectAnimator.ofFloat(window.getDecorView(),
+                    "alpha", 0f, 1f);
+            appearing.setInterpolator(new AccelerateInterpolator());
+            appearing.setDuration(300);
+            appearing.start();
         }
     }
 }
