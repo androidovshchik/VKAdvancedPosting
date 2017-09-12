a48e9b5cfc67784bdc329d4236e19489fbfb7e5c
Vlad <gimrcpp@gmail.com>
2017-09-12 14:22:31 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/components/VKLoginCallback.java
+++ android/src/rf/androidovshchik/vkadvancedposting/components/VKLoginCallback.java
@@ -6,6 +6,7 @@ import com.vk.sdk.api.VKError;
 
 import rf.androidovshchik.vkadvancedposting.events.VKLoginEvent;
 import rf.androidovshchik.vkadvancedposting.utils.EventUtil;
+import timber.log.Timber;
 
 public class VKLoginCallback implements VKCallback<VKAccessToken> {
 
@@ -16,6 +17,7 @@ public class VKLoginCallback implements VKCallback<VKAccessToken> {
 
     @Override
     public void onError(VKError error) {
+        Timber.e(error.errorMessage);
         EventUtil.post(new VKLoginEvent(false));
     }
 }
--- android/src/rf/androidovshchik/vkadvancedposting/components/VKTokenTracker.java
+++ android/src/rf/androidovshchik/vkadvancedposting/components/VKTokenTracker.java
@@ -3,13 +3,15 @@ package rf.androidovshchik.vkadvancedposting.components;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 
-import rf.androidovshchik.vkadvancedposting.events.VKLoginEvent;
+import rf.androidovshchik.vkadvancedposting.events.InvalidTokenEvent;
 import rf.androidovshchik.vkadvancedposting.utils.EventUtil;
 
 public class VKTokenTracker extends VKAccessTokenTracker {
 
     @Override
     public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
-        EventUtil.post(new VKLoginEvent(newToken == null));
+        if (newToken == null) {
+            EventUtil.post(new InvalidTokenEvent());
+        }
     }
 }
