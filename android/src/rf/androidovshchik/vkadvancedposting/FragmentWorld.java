a7c6ef83037068a597c85036add0d2c52e6d8f31
Vlad <gimrcpp@gmail.com>
2017-09-15 15:07:55 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/FragmentWorld.java
+++ android/src/rf/androidovshchik/vkadvancedposting/FragmentWorld.java
@@ -1,11 +1,14 @@
 package rf.androidovshchik.vkadvancedposting;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
+
+import rf.androidovshchik.vkadvancedposting.utils.ViewUtil;
 
 public class FragmentWorld extends AndroidFragmentApplication {
 
@@ -14,7 +17,16 @@ public class FragmentWorld extends AndroidFragmentApplication {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        world = new World();
+        Point screen = ViewUtil.getScreen(getContext().getApplicationContext());
+        Point window = ViewUtil.getWindow(getContext().getApplicationContext());
+        int worldWidth = getResources().getDimensionPixelSize(R.dimen.world_width);
+        int worldHeight;
+        if (screen.y > screen.x) {
+            worldHeight = Math.round(1f * screen.y * worldWidth / screen.x);
+        } else {
+            worldHeight = Math.round(1f * screen.x * worldWidth / screen.y);
+        }
+        world = new World(window.y, worldWidth, worldHeight);
         return initializeForView(world);
     }
 }
--- android/src/rf/androidovshchik/vkadvancedposting/utils/ViewUtil.java
+++ android/src/rf/androidovshchik/vkadvancedposting/utils/ViewUtil.java
@@ -3,8 +3,14 @@ package rf.androidovshchik.vkadvancedposting.utils;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Point;
+import android.os.Build;
 import android.view.Display;
 import android.view.WindowManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import timber.log.Timber;
 
 public final class ViewUtil {
 
@@ -15,11 +21,36 @@ public final class ViewUtil {
     }
 
     @SuppressWarnings("unused")
-    public static Point getScreen(Context context) {
+    public static Point getWindow(Context context) {
         WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = manager.getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
+        return size;
+    }
+
+    @SuppressWarnings("all")
+    public static Point getScreen(Context context) {
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        Point size = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealSize(size);
+        } else {
+            Method methodGetRawHeight, methodGetRawWidth;
+            try {
+                methodGetRawHeight = Display.class.getMethod("getRawHeight");
+                methodGetRawWidth = Display.class.getMethod("getRawWidth");
+                size.y = (Integer) methodGetRawHeight.invoke(display);
+                size.x = (Integer) methodGetRawWidth.invoke(display);
+            } catch (NoSuchMethodException e) {
+                Timber.e(e.getMessage());
+            } catch (IllegalAccessException e) {
+                Timber.e(e.getMessage());
+            } catch (InvocationTargetException e) {
+                Timber.e(e.getMessage());
+            }
+        }
         return size;
     }
 }
