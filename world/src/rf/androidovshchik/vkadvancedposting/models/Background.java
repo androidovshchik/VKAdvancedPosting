1c8356ab4c7f11bb20ae74eceb4b9f5cb7e500d7
Vlad <gimrcpp@gmail.com>
2017-09-16 17:58:14 +0300

    changes

--- world/src/rf/androidovshchik/vkadvancedposting/models/Background.java
+++ world/src/rf/androidovshchik/vkadvancedposting/models/Background.java
@@ -1,13 +1,47 @@
 package rf.androidovshchik.vkadvancedposting.models;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 
-public abstract class Background extends Player {
+public class Background extends Player {
 
-    public Background(int index, Texture texture) {
-        super(index, texture);
-        setVisible(false);
+    private static final String TAG = Background.class.getSimpleName();
+
+    public Background(Texture texture, int type, String path) {
+        super(texture, type, path);
     }
 
-    public abstract void bind(int worldWidth, int worldHeight);
+    public Background(Texture texture, Record record) {
+        super(texture, record);
+    }
+
+    public void setup(int worldWidth, int worldHeight) {
+        setOrigin(getWidth() / 2, getHeight() / 2);
+        setX(worldWidth / 2 - getWidth() / 2);
+        setY(worldHeight / 2 - getHeight() / 2);
+        setScale(1f * worldHeight / getHeight());
+    }
+
+    @Override
+    public void onModeTranslation(boolean isPostMode, int worldHeight, int worldHalfDifference) {
+        if (type == TYPE_CENTER_BACKGROUND) {
+            return;
+        }
+        MoveToAction moveToAction = moveToPool.obtain();
+        moveToAction.setPool(moveToPool);
+        switch (type) {
+            case TYPE_TOP_BACKGROUND:
+                moveToAction.setPosition(getX(),
+                        getY() + worldHalfDifference * (isPostMode ? - 1 : 1));
+                break;
+            case TYPE_BOTTOM_BACKGROUND:
+                moveToAction.setPosition(getX(),
+                        getY() + worldHalfDifference * (isPostMode ? 1 : - 1));
+                break;
+            default:
+                break;
+        }
+        moveToAction.setDuration(ANIMATION_TIME_MODE_TRANSLATION);
+        addAction(moveToAction);
+    }
 }
--- world/src/rf/androidovshchik/vkadvancedposting/models/Player.java
+++ world/src/rf/androidovshchik/vkadvancedposting/models/Player.java
@@ -3,14 +3,50 @@ package rf.androidovshchik.vkadvancedposting.models;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class Player extends Image {
+import rf.androidovshchik.vkadvancedposting.pools.MoveToPool;
 
-    public static final int NONE = -1;
+public abstract class Player extends Image {
 
-    public int index;
+    public static final int TYPE_STICKER = 1;
 
-    public Player(int index, Texture texture) {
+    public static final int TYPE_TOP_CURTAIN = 10;
+    public static final int TYPE_BOTTOM_CURTAIN = 11;
+
+    public static final int TYPE_TOP_BACKGROUND = 20;
+    public static final int TYPE_CENTER_BACKGROUND = 21;
+    public static final int TYPE_BOTTOM_BACKGROUND = 22;
+
+    protected static final float ANIMATION_TIME_MODE_TRANSLATION = 0.3f;
+
+    protected static MoveToPool moveToPool = new MoveToPool();
+
+    public int type;
+
+    public String path;
+
+    public Player(Texture texture, int type, String path) {
         super(texture);
-        this.index = index;
+        this.type = type;
+        this.path = path;
+    }
+
+    public Player(Texture texture, Record record) {
+        super(texture);
+        this.type = record.type;
+        this.path = record.path;
+    }
+
+    public abstract void onModeTranslation(boolean isPostMode, int worldHeight,
+                                           int worldHalfDifference);
+
+    public Record save2Record() {
+        Record record = new Record();
+        record.type = type;
+        record.path = path;
+        record.x = getX();
+        record.y = getY();
+        record.scale = getScaleX();
+        record.rotation = getRotation();
+        return record;
     }
 }
