0fa355ceef1d56138527f4c492f1092318523ac8
Vlad <gimrcpp@gmail.com>
2017-09-16 18:06:27 +0300

    changes

---
+++ world/src/rf/androidovshchik/vkadvancedposting/models/Record.java
@@ -0,0 +1,15 @@
+package rf.androidovshchik.vkadvancedposting.models;
+
+import java.io.Serializable;
+
+public class Record implements Serializable {
+
+    public int type;
+
+    public String path;
+
+    public float x;
+    public float y;
+    public float scale;
+    public float rotation;
+}
--- world/src/rf/androidovshchik/vkadvancedposting/models/Sticker.java
+++ world/src/rf/androidovshchik/vkadvancedposting/models/Sticker.java
@@ -2,38 +2,31 @@ package rf.androidovshchik.vkadvancedposting.models;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
-import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
 import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
 
 import java.util.Random;
 
 import rf.androidovshchik.vkadvancedposting.components.GdxLog;
-import rf.androidovshchik.vkadvancedposting.pools.MoveToPool;
 import rf.androidovshchik.vkadvancedposting.pools.RotateToPool;
 import rf.androidovshchik.vkadvancedposting.pools.ScaleToPool;
-
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.parallel;
 
 public class Sticker extends Player {
 
     private static final String TAG = Sticker.class.getSimpleName();
 
+    private static final float ANIMATION_TIME_APPEAR = 0.15f;
+
+    public static final int MAX_COUNT = 100;
+
+    public static final float MIN_SCALE = 0.4f;
+    public static final float MAX_SCALE = 2.0f;
+
     private static Random random = new Random();
 
-    private static MoveToPool moveToPool = new MoveToPool();
     private static ScaleToPool scaleToPool = new ScaleToPool();
     private static RotateToPool rotateToPool = new RotateToPool();
 
-    public static final int NONE = -1;
-
-    public static final float MAX_SCALE = 2.0f;
-    public static final float MIN_SCALE = 0.4f;
-
     public boolean isPinching = false;
-
-    public float baseRotation = 0.0f;
-    public float cropAspectRatio;
-    public float defaultScale = 1.0f;
 
     public float startDragX;
     public float startDragY;
@@ -43,10 +36,10 @@ public class Sticker extends Player {
     public float startScale;
     public float startRotation;
 
-    public Sticker(int index, Texture texture, int worldWidth, int worldHeight) {
-        super(index, texture);
-        GdxLog.d(TAG, "%d sticker", index);
+    public Sticker(Texture texture, int type, String path, int worldWidth, int worldHeight) {
+        super(texture, type, path);
         setOrigin(getWidth() / 2, getHeight() / 2);
+        setScale(MIN_SCALE);
         // 0.6f < scale < 0.8f
         startScale = 1f * random.nextInt(20) / 100 + 0.6f;
         float realWidth = getWidth() * startScale;
@@ -55,7 +48,6 @@ public class Sticker extends Player {
         float halfHeightDifference = getHeight() * (1f - startScale) / 2;
         float x, y;
         boolean leftPosition;
-        // x
         if (random.nextFloat() > 0.5f) {
             leftPosition = false;
             x = worldWidth - halfWidthDifference - realWidth * (1f + random.nextFloat() / 3);
@@ -63,7 +55,7 @@ public class Sticker extends Player {
             leftPosition = true;
             x = random.nextFloat() * realWidth / 3 - halfWidthDifference;
         }
-        // y
+        setX(x);
         if (random.nextFloat() > 0.5f) {
             startRotation = leftPosition ? - random.nextInt(45) : random.nextInt(45);
             y = worldHeight - halfHeightDifference - realHeight * (1f + random.nextFloat() / 3);
@@ -71,9 +63,9 @@ public class Sticker extends Player {
             startRotation = leftPosition ? random.nextInt(45) : - random.nextInt(45);
             y = random.nextFloat() * realHeight / 3 - halfHeightDifference;
         }
-        setPosition(x, y);
-        setScale(MIN_SCALE);
         setRotation(startRotation);
+        setY(y);
+        onAppear();
     }
 
     public void setDragStarts(float x, float y) {
@@ -85,8 +77,6 @@ public class Sticker extends Player {
     public void setPinchStarts(float x, float y) {
         startPinchX = x;
         startPinchY = y;
-        startScale = getScaleX();
-        startRotation = getRotation();
         GdxLog.f(TAG, "setPinchStarts startPinchX: %f startPinchY: %f startScale: %f startRotation: %f",
                 startPinchX, startPinchY, startScale, startRotation);
     }
@@ -95,12 +85,28 @@ public class Sticker extends Player {
         ScaleToAction scaleToAction = scaleToPool.obtain();
         scaleToAction.setPool(scaleToPool);
         scaleToAction.setScale(startScale);
-        scaleToAction.setDuration(0.15f);
+        scaleToAction.setDuration(ANIMATION_TIME_APPEAR);
         addAction(scaleToAction);
     }
 
-    public void onPinch(float x, float y, float scale, float rotation) {
+    @Override
+    public void onModeTranslation(boolean isPostMode, int worldHeight, int worldHalfDifference) {
+        float centerY = getY() + getHeight() / 2;
         MoveToAction moveToAction = moveToPool.obtain();
+        moveToAction.setPool(moveToPool);
+        if (isPostMode) {
+            moveToAction.setPosition(getX(),
+                    getY() + worldHalfDifference * (centerY < worldHeight / 2 ? 1 : -1));
+        } else {
+            moveToAction.setPosition(getX(),
+                    getY() + worldHalfDifference * (centerY < worldHeight / 2 ? -1 : 1));
+        }
+        moveToAction.setDuration(ANIMATION_TIME_MODE_TRANSLATION);
+        addAction(moveToAction);
+    }
+
+    public void onPinch(float x, float y, float scale, float rotation) {
+        /*MoveToAction moveToAction = moveToPool.obtain();
         moveToAction.setPool(moveToPool);
         moveToAction.setPosition(x, y);
         ScaleToAction scaleToAction = scaleToPool.obtain();
@@ -109,6 +115,6 @@ public class Sticker extends Player {
         RotateToAction rotateToAction = rotateToPool.obtain();
         rotateToAction.setPool(rotateToPool);
         rotateToAction.setRotation(rotation);
-        addAction(parallel(moveToAction, scaleToAction, rotateToAction));
+        addAction(parallel(moveToAction, scaleToAction, rotateToAction));*/
     }
 }
