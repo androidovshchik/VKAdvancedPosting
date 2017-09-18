ac8f8decd8692d2ebcac30c6230d3fef41793baa
Vlad <gimrcpp@gmail.com>
2017-09-18 10:50:44 +0300

    changes

--- android/src/rf/androidovshchik/vkadvancedposting/ActivityMainWorld.java
+++ android/src/rf/androidovshchik/vkadvancedposting/ActivityMainWorld.java
@@ -1,18 +1,75 @@
 package rf.androidovshchik.vkadvancedposting;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.PopupWindow;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import rf.androidovshchik.vkadvancedposting.events.clicks.PhotoClickEvent;
+import rf.androidovshchik.vkadvancedposting.events.clicks.StickerClickEvent;
+import rf.androidovshchik.vkadvancedposting.utils.ViewUtil;
+import rf.androidovshchik.vkadvancedposting.views.layout.MainLayout;
+import rf.androidovshchik.vkadvancedposting.views.recyclerview.photos.PhotosRecyclerView;
 
 public abstract class ActivityMainWorld extends ActivityMainBase {
 
+	@BindView(R.id.mainLayout)
+	public MainLayout mainLayout;
+
+	protected Rect rectResizedWindow;
+	protected int windowHeight;
+
 	protected FragmentWorld fragment;
+
+	protected PopupWindow photosPopup;
+	protected PopupWindow stickersPopup;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		rectResizedWindow = new Rect();
+		windowHeight = ViewUtil.getWindow(getApplicationContext()).y;
 		fragment = new FragmentWorld();
 		getSupportFragmentManager()
 				.beginTransaction()
-				.replace(R.id.world, fragment, FragmentWorld.class.getSimpleName())
+				.add(R.id.world, fragment, FragmentWorld.class.getSimpleName())
 				.commitAllowingStateLoss();
+	}
+
+	@Override
+	public void onGlobalLayout() {
+		getWindow().getDecorView().getWindowVisibleDisplayFrame(rectResizedWindow);
+		int difference = windowHeight - rectResizedWindow.bottom;
+		mainLayout.onViewportChange(difference);
+	}
+
+	@OnClick(R.id.actionPost)
+	public void onPost() {
+		mainLayout.onPostMode();
+	}
+
+	@OnClick(R.id.actionHistory)
+	public void onHistory() {
+		mainLayout.onHistoryMode();
+	}
+
+	@SuppressWarnings("unused")
+	@Subscribe(threadMode = ThreadMode.POSTING)
+	public void onStickerClickEvent(StickerClickEvent event) {
+		fragment.world.postRunnable("addSticker", event.position + 1);
+	}
+
+	@SuppressWarnings("unused")
+	@Subscribe(threadMode = ThreadMode.POSTING)
+	public void onPhotoClickEvent(PhotoClickEvent event) {
+		String sdcardPath = Environment.getExternalStorageDirectory().getPath() + "/";
+		String path = ((PhotosRecyclerView) photosPopup.getContentView()).adapterPhotos
+				.photoPaths.get(event.position).getPath().substring(sdcardPath.length());
+		fragment.world.postRunnable("setPhotoBackground", path);
 	}
 }
